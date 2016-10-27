503690467f6ebceae7ab77cfddf548242d19126c
Jordi Pont-Tuset <jponttuset@gmail.com>
2016-10-27 17:34:59 +0200

    - Solving script

--- experiments/eval_all.py
+++ experiments/eval_all.py
@@ -1,5 +1,6 @@
 import os
 techniques = ['mcg','sf-lab','sf-mot','nlc','cvos','trc','msg','key','sal','fst','tsp','sea','hvs','jmp','fcp','bvs'];
+gt_set = 'val'
 for ii in range(0,len(techniques)):
-    os.system('qsub -N davis-'+techniques[ii]+' eval_all.py ' + techniques[ii] +' "{\'F\',\'J\',\'T\'}" val')
+    os.system('qsub -N '+techniques[ii]+'-'+gt_set+' eval_one.py ' + techniques[ii] +' "{\'F\',\'J\',\'T\'}" ' + gt_set)
 
