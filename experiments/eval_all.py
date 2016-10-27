5da7873dcb5cf830c8f5b308d6faaae3e3a73edd
Jordi Pont-Tuset <jponttuset@gmail.com>
2016-10-27 16:55:02 +0200

    - Script to eval all in cluster

--- experiments/eval_all.py
+++ experiments/eval_all.py
@@ -3,7 +3,7 @@
 # ----- Parameters passed to the cluster -------
 #$ -S /usr/bin/python
 #$ -l h_rt=5:59:00
-#$ -l h_vmem=15000M
+#$ -l h_vmem=5000M
 #$ -o /scratch_net/neo/jpont/logs/
 #$ -j y
 
@@ -14,63 +14,20 @@ import math
 import sys
 
 
-# ---------------- Functions -------------------
-def subfolders(parent_dir):
-    return [name for name in os.listdir(parent_dir)
-            if os.path.isdir(os.path.join(parent_dir, name))]
-
-
 # ------------- Get the parameters -------------
 if len(sys.argv)<3:
-  print "Usage 'davis_segment im_root_folder results_root_folder'"
+  print "Usage 'eval_all technique measures gt_set'"
   exit(1)
 
-database_folder = sys.argv[1]
-results_folder  = sys.argv[2]
-
-# Get subfolders
-folders = subfolders(database_folder);
-
-# Which subfolder we process?
-task_id = int(os.getenv("SGE_TASK_ID", "0"))
-if task_id>len(folders):
-  print "Number of workers should be less than " + str(len(folders))
-  exit(1)
-if task_id==0:
-  print "SGE_TASK_ID not set"
-  exit(1)
+technique = sys.argv[1]
+measures  = sys.argv[2]
+gt_set    = sys.argv[3]
 
 # ---------------- Check main folders -------------------
-ucm_folder = results_folder+"/ucm"
-prop_folder = results_folder+"/proposals"
-
-if not os.path.exists(results_folder):
-  if task_id==1:
-    os.makedirs(results_folder)
-if not os.path.exists(ucm_folder):
-  if task_id==1:
-    os.makedirs(ucm_folder)
-if not os.path.exists(prop_folder):
-  if task_id==1:
-    os.makedirs(prop_folder)
-
-
-# ---------------- Check current folders -------------------
-im_folder  = database_folder+"/"+folders[task_id-1];
-curr_ucm_folder = ucm_folder+"/"+folders[task_id-1];
-curr_prop_folder = prop_folder+"/"+folders[task_id-1];
-
-if not os.path.exists(curr_ucm_folder):
-    os.makedirs(curr_ucm_folder)
-if not os.path.exists(curr_prop_folder):
-    os.makedirs(curr_prop_folder)
-
-
-print "Processing: " + im_folder
 
 # Run the actual code
-os.chdir('/srv/glusterfs/jpont/dev/scripts')
-command_to_run = "/usr/sepp/bin/matlab -nodesktop -nodisplay -nosplash -r \"mcg_folder('"+im_folder+"','accurate','"+curr_prop_folder+"','"+curr_ucm_folder+"');exit\""
+os.chdir('/srv/glusterfs/jpont/dev/davis-matlab')
+command_to_run = "/usr/sepp/bin/matlab -nodesktop -nodisplay -nosplash -r \"startup;eval_result('"+technique+"','"+measures+"','"+gt_set+"');exit\""
 os.system(command_to_run)
 
 
