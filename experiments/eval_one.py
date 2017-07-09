e86bfad9223d0a1abfae0a8656558fd70fe86921
Jordi Pont-Tuset <jponttuset@gmail.com>
2017-07-09 18:17:49 +0200

    - Minor bug fixes for 2017

--- experiments/eval_one.py
+++ experiments/eval_one.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
+# qsub -N OSVOS eval_one.py OSVOS "{'F','J'}" val-2016
 # ----- Parameters passed to the cluster -------
 #$ -S /usr/bin/python
-#$ -l h_rt=5:59:00
+#$ -l h_rt=23:59:00
 #$ -l h_vmem=30000M
 #$ -o /scratch_net/neo/jpont/logs/
 #$ -j y
@@ -16,7 +17,7 @@ import sys
 
 # ------------- Get the parameters -------------
 if len(sys.argv)<3:
-  print "Usage 'eval_all technique measures gt_set'"
+  print "Usage 'eval_one technique measures gt_set'"
   exit(1)
 
 technique = sys.argv[1]
@@ -26,8 +27,8 @@ gt_set    = sys.argv[3]
 # ---------------- Check main folders -------------------
 
 # Run the actual code
-os.chdir('/srv/glusterfs/jpont/dev/davis-matlab')
-command_to_run = "/usr/sepp/bin/matlab -nodesktop -nodisplay -nosplash -r \"startup;eval_result('"+technique+"',"+measures+",'"+gt_set+"');exit\""
+os.chdir('/scratch_net/neo_second/jpont/dev/davis-matlab')
+command_to_run = "/usr/sepp/bin/matlab -nodesktop -nodisplay -nosplash -r \"startup;db_set_properties(2017,1,'480p');eval_result('"+technique+"',"+measures+",'"+gt_set+"');exit\""
 os.system(command_to_run)
 
 
