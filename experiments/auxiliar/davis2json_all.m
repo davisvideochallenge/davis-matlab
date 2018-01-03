1a27a8979ed8966b45c82c3b0b26d07748398bf8
Jordi Pont-Tuset <jponttuset@gmail.com>
2018-01-03 17:30:09 +0100

    - One technique output

--- experiments/auxiliar/davis2json_all.m
+++ experiments/auxiliar/davis2json_all.m
@@ -3,9 +3,15 @@ gt_set = 'val';
 
 res_folder = '.';
 
+%% All techniques
 for ii=1:length(techniques)
     res_file = fullfile(res_folder,[strrep(techniques{ii},'-',''), '_davis.json']);
     davis2json(techniques{ii},res_file,gt_set);
 end
 
-% REMEMBER TO SET THE FILES TO READABLE BY ALL USERS
+%% One technique in particular
+tec = 'sfls';
+res_file = fullfile(res_folder,[strrep(tec,'-',''), '_davis.json']);
+davis2json(tec,res_file,gt_set);
+
+%% REMEMBER TO SET THE FILES TO READABLE BY ALL USERS
